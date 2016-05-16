daeab29d3303ac85753ee8b08d509772017ad960
GrahamSM <graham.sm94@gmail.com>
2016-05-16 16:53:30 +0000

    Added track posts, wall view

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,32 @@
 get '/' do
   erb :index
 end
+
+get '/posts' do
+	@tracks = Track.all
+	erb :'posts/index'
+end
+
+get '/posts/new' do
+	@track = Track.new
+	erb :'posts/new'
+end
+
+get '/posts/:id' do
+	@track = Track.find params[:id]
+	erb :'posts/show'
+end
+
+post '/posts' do
+	@track = Track.new(
+	title: params[:title],
+  artist: params[:artist],
+	url: params[:url],
+	author: params[:author]
+	)
+	if @track.save
+		redirect '/posts'
+	else
+		erb :'posts/new'
+	end
+end
